b8d38e5f9810fd10a40c3f9c5beea698134a1cab
Taemin Lee <persuade@gmail.com>
2020-10-19 16:23:43 +0900

    add split_chunks

--- kss.pyx
+++ kss.pyx
@@ -2,6 +2,8 @@ from libcpp.string cimport string
 from libcpp.vector cimport vector
 
 from typing import List
+from collections import namedtuple
+import math
 
 cdef extern from "sentence_splitter.h":
     vector[string] splitSentences(string)
@@ -14,3 +16,34 @@ def split_sentences(str) -> List[str]:
         results.append(r.decode('utf-8'))
 
     return results
+
+SentenceIndex = namedtuple('SentenceIndex', ['start', 'end'])
+ChunkWithIndex = namedtuple('ChunkWithIndex', ['start', 'text'])
+
+def split_sentences_index(text) -> List[SentenceIndex]:
+    def get_sentence_index(sentence):
+        return SentenceIndex(text.index(sentence), text.index(sentence) + len(sentence))
+    sentences = split_sentences(text)
+    return [get_sentence_index(sentence) for sentence in sentences]
+
+
+def split_chunks(text: str, max_length=128, overlap=True, indexes=None) -> List[ChunkWithIndex]:
+    def get_chunk_with_index():
+        start = span[0].start
+        end = span[-1].end
+        return ChunkWithIndex(span[0].start, text[start:end])
+    if(indexes is None):
+        indexes = split_sentences_index(text)
+    span = []
+    chunks = []
+    for index in indexes:
+        if(len(span) > 0):
+            if(index.end - span[0].start > max_length):  # len = last_end - first_start
+                chunks.append(get_chunk_with_index())
+                if(overlap):
+                    span = span[math.trunc(len(span)/2):]  # cut half
+                else:
+                    span = []
+        span.append(index)
+    chunks.append(get_chunk_with_index())
+    return chunks
